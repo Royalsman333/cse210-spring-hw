78f4bee65b4bf830858b69c967ad7389249c9689
Royalsman333 <146398098+Royalsman333@users.noreply.github.com>
2024-04-28 00:05:18 -0600

    C Sharp Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your Grade Percentage: ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your current grade is: {letter}");
+
+        if(percent >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class!");
+        }
     }
 }
